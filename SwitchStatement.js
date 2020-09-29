e20a90584005f749701a788317979611c78f42e5
katfernandez22 <60719712+katfernandez22@users.noreply.github.com>
2020-09-28 20:13:23 -0400

    Add files via upload

---
+++ SwitchStatement.js
@@ -0,0 +1,22 @@
+let favoriteMovie = "Star Wars";
+let moviePhrase;
+
+switch (favoriteMovie) {
+  case "Jaws":
+    moviePhrase = "You're gonna need a bigger boat.";
+    break;
+  case "The Shining":
+    moviePhrase = "All work and no play makes Jack a dull boy.";
+    break;
+  case "Star Wars":
+    moviePhrase = "Do. Or do not. There is no try.";
+    break;
+  case "Forrest Gump":
+    moviePhrase = "Life was like a box of chocolates.";
+    break;
+  case "Back to the future":
+    moviePhrase = "Where we're going, we don't need roads.";
+    break;
+  default:
+    moviePhrase = "Great movie choice!";
+}
